a84b14c8c6f60b6417270596efcf1d7b535a872a
Sully <zhengjie@bcinnovationlabs.com>
2019-11-18 21:06:12 -0500

    text

--- indoorair/sensor/templates/retrieve/retrieve.js
+++ indoorair/sensor/templates/retrieve/retrieve.js
@@ -1,34 +1,18 @@
-function generateViewFromObject(dataObj) {
-    if (dataObj.was_found === false) {
-        alert("Sorry we could not find that sensor!");
-        onBackClick();
-    } else {
-        var idInputElement = document.getElementById("id");
-        var nameInputElement = document.getElementById("name");
-        idInputElement.value = dataObj.id;
-        nameInputElement.value = dataObj.name;
-    }
-}
+function onRetrieveClick() {
 
-function onPageLoadRunGetInstrumentDetailsFromAPI(instrument_id) {
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            const dataString = this.responseText;
-            const dataObj = JSON.parse(dataString);
-            generateViewFromObject(dataObj);
-        }
-    }
-    const detailURL = "/api/sensor/"+instrument_id.toString();
-    console.log(detailURL);
-    xhttp.open("GET", detailURL, true);
-    xhttp.send();
-}
+  var xhttp = new XMLHttpRequest();
+   xhttp.onreadystatechange = function() {
+     if (this.readyState == 4 && this.status == 200) { // Thisis the callback function
+         // Get the string data that the server sent us.
+         if (this.readyState == 4 && this.status == 200) {
 
-const sensor_id = {{ sensor_id }};
-onPageLoadRunGetInstrumentDetailsFromAPI(sensor_id);
+           document.getElementById('temp_avg').innerHTML = this.responseText;
 
+         }
+       }
+     }
+       xhttp.open('POST', "{% url 'retrieve_profile_api' %}", true);
+       xhttp.setRequestHeader("content-type", "application/x-www-form-urlencoded");
+       xhttp.send();
 
-function onBackClick() {
-    window.location.href = "{% url 'i_retrieve_page' %}";
 }
